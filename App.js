4db943262cddc87ca24b80550da5336e2495c78f
unknown <kushalcd@gmail.com>
2021-01-30 03:12:15 -0600

    fixed another merge conflict

--- App.js
+++ App.js
@@ -2,15 +2,9 @@ import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import navigationTheme from "./app/navigation/navigationTheme";
 import AppNavigator from "./app/navigation/AppNavigator";
-<<<<<<< HEAD
-
-import Amplify, { Auth } from 'aws-amplify';
-import awsconfig from './src/aws-exports';
-=======
 import AuthNavigator from "./app/navigation/AuthNavigator";
 import Amplify, { Auth } from "aws-amplify";
 import awsconfig from "./src/aws-exports";
->>>>>>> 3b8fa04ad4840d5be915dfe76f74657c6565385f
 Amplify.configure(awsconfig);
 
 export default function App() {
